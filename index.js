3407b329c8c40cf701b7f2205f7e327fe476cd79
Reshilobeth <92940855+Reshilobeth@users.noreply.github.com>
2021-12-29 10:47:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let userInfo = [
+    {
+        ID:1234,
+        Firstname:"Reshil",
+        Lastname:"obeth",
+        age:20,
+        profession:"student",
+        city:"Chenni",
+        phoneNumber:1234567890,
+    }
+]
+
+for(i =0; i< userInfo.length; i++){
+    console.log(userInfo[i]);
+}
